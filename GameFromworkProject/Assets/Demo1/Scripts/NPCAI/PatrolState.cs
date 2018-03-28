4b3eee6a1b2218dde66344b2142641d97e3563e4
yangyaobin <yangyaobin@3dov.cn>
2018-03-28 09:42:30 +0800

    完成有限状态机（FSM）的学习； 完成Protocol Buffer-net的学习。

---
+++ GameFromworkProject/Assets/Demo1/Scripts/NPCAI/PatrolState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : FSMState
+{
+    private int targetWayPoint = 0;
+    private Transform[] wayPoints;
+    private GameObject npc;
+    private GameObject player;
+    private Rigidbody npcRd;
+
+    public PatrolState(Transform[] wp,GameObject npc,GameObject player)
+    {
+        stateID = StateID.Patrol;
+        this.npc = npc;
+        this.player = player;
+        wayPoints = wp;
+        npcRd = npc.GetComponent<Rigidbody>();
+    }
+
+    public override void DoBeforeEntering()
+    {
+        Debug.Log("Entering state "+ID);
+    }
+
+    public override void DoUpdate()
+    {
+        CheckTransition();
+        PatrolMove();
+    }
+
+    //检查转换条件
+    private void CheckTransition()
+    {
+        if (Vector3.Distance(player.transform.position, npc.transform.position) < 5)
+        {
+            fsm.PerformTransition(Transition.SawPlayer);
+        }
+    }
+
+    private void PatrolMove()
+    {
+        npcRd.velocity = npc.transform.forward * 3f;
+        Transform targetTran = wayPoints[targetWayPoint];
+        Vector3 targetPosition = targetTran.position;
+        targetPosition.y = npc.transform.position.y;
+        npc.transform.LookAt(targetPosition);
+        if (Vector3.Distance(npc.transform.position, targetPosition) < 1)
+        {
+            targetWayPoint++;
+            targetWayPoint %= wayPoints.Length;
+        };
+    }
+}
---
+++ GameFromworkProject/Assets/Demo2 protobufnet/TestProtobuf.cs
@@ -0,0 +1,50 @@
+﻿using ProtoBuf;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestProtobuf : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        /*ProtoBuf序列化
+        User user = new User();
+        user.ID = 100;
+        user.Username = "siki";
+        user.Password = "123456";
+        user.Level = 100;
+        user._UserType = User.UserType.Master;
+
+        //FileStream fs = File.Create(Application.dataPath + "/user.bin");
+        //Serializer.Serialize<User>(fs, user);
+        //fs.Close();
+
+        using (var fs = File.Create(Application.dataPath + "/user.bin"))
+        {
+            Serializer.Serialize<User>(fs, user);
+        }
+        */
+
+        /*ProtoBuf反序列化
+        User user = null;
+        using (var fs = File.OpenRead(Application.dataPath + "/user.bin"))
+        {
+            user=Serializer.Deserialize<User>(fs);
+        }
+        print(user.ID);
+        print(user.Level);
+        print(user.Password);
+        print(user.Username);
+        print(user._UserType);
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
